261ba432085bd2c67b177287ebd63ed94e257eac
Pobitrodeb <pobitrodn@gmail.com>
2023-01-31 23:24:39 +0600

    file include and index html and router install

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './route';
+import store from './store'; 
 
-createApp(App).mount('#app')
+createApp(App).use(store).user(router).mount('#app')
